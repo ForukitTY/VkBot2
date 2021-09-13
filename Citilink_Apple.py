438b5f5067638e0aac8b6c0603719476799a87d5
Mukhamed <cool.gedgafov@mail.ru>
2021-09-13 13:08:22 +0300

    set_cookie

--- Citilink_Apple.py
+++ Citilink_Apple.py
@@ -11,7 +11,13 @@ def pars_citilink():
 		  {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.43 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36 OPR/75.0.3969.243'},
 		  {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.43 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36 OPR/75.0.3969.243'},
 		  {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.43 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 OPR/76.0.4017.123'}]
-	head={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.43 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36 OPR/76.0.4017.94', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}
+	head={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.43 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36 OPR/76.0.4017.94',
+	   'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive',
+	   'cache-control': 'no-cache, private',
+	   
+	   'set-cookie': 'old_design=0; expires=Tue; Max-Age=31536000; path=/; domain=.citilink.ru, _tuid=3a9ce050ca86154d821a18b944ffde39ede4c92a; expires=Tue; Max-Age=31536000; path=/; domain=.citilink.ru, _space=ast_cl%3A; expires=Tue; Max-Age=31536000; path=/; domain=.citilink.ru, ab_test=90x10v4%3A1%7Creindexer%3A2%7Cnew_designv10%3A9%7Cnew_designv13%3A1%7Cproduct_card_design%3A3%7Cdummy%3A20; expires=Mon; Max-Age=7862400; path=/; domain=.citilink.ru; httponly, ab_test_analytics=90x10v4%3A1%7Creindexer%3A2%7Cnew_designv10%3A9%7Cnew_designv13%3A1%7Cproduct_card_design%3A3%7Cdummy%3A20; expires=Mon; Max-Age=7862400; path=/; domain=.citilink.ru, rr_rcs=deleted; expires=Sun; Max-Age=0; path=/; domain=citilink.ru', 
+	   'content-encoding': 'gzip'
+	   }
 
 	prox=[{'http':'http://217.29.53.64:33165', 'https':'https://217.29.53.64:33165'}, {'http':'socks5://20.182.253.244:11857','https': 'socks5://20.182.253.244:11857'}, {'http':'http://45.90.216.166:10001','https': 'https://45.90.216.166:10001'}]
 	print("\n______________________CitiLink____________________")
@@ -24,7 +30,7 @@ def pars_citilink():
 
 		req=requests.get(f'https://www.citilink.ru/catalog/smartfony/?f=discount.any%2Crating.any%2Capple%2Chonor%2Csamsung%2Crating.any%2Capple%2Csamsung&p={iter}',headers=head, timeout=35)
 		#req=requests.get(f'https://2ip.ua/ru/',headers=agents[2], timeout=37)
-		costAndTags = BeautifulSoup(req.text, "html.parser").find_all(attrs={'class': 'ipblockgradient'})
+		#costAndTags = BeautifulSoup(req.text, "html.parser").find_all(attrs={'class': 'ipblockgradient'})
 
 		print(req.status_code,'\n-----------', req.request.headers, req.headers )
 		#print(costAndTags,'\n', '\n') 
