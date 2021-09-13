855a691d09d5731861744f0561a157de413863a4
Mukhamed <cool.gedgafov@mail.ru>
2021-09-13 11:53:45 +0300

    только юзер агент

--- Citilink_Apple.py
+++ Citilink_Apple.py
@@ -5,30 +5,38 @@ from bs4 import BeautifulSoup
 def pars_citilink():
 	dictOriginalMvideo={}
 	kolvotelephonov=0
-	head=[{'User-Agent':'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 YaBrowser/21.8.1.476 Yowser/2.5 Safari/537.36'},
+	agents=[{'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36 RuxitSynthetic/1.0 v7139868079105931835 t8052286838287810618'},
 		  {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.43 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36 OPR/76.0.4017.94'},
 		  {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.43 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36 OPR/76.0.4017.94'},
 		  {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.43 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36 OPR/75.0.3969.243'},
 		  {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.43 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36 OPR/75.0.3969.243'},
 		  {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.43 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 OPR/76.0.4017.123'}]
-	prox=[{'http':'http://184.178.172.13:15311'},
-	   {'http':'http://184.178.172.13:15311'},
-	   {'http':'http://139.198.179.174:3128'},
-	   {'http':'http://253.6.201.182:53281'},
-	   {'http':'http://250.38.65.0:999'},
-	   {'http':'http://105.123.68.183:8080'}]
+	head={
+        'Connection':'keep-alive',
+
+
+        'Upgrade-Insecure-Requests':'1',
+
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.43 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36 OPR/76.0.4017.94'
+		}
+
+	prox=[{'http':'http://217.29.53.64:33165', 'https':'https://217.29.53.64:33165'}, {'http':'socks5://20.182.253.244:11857','https': 'socks5://20.182.253.244:11857'}, {'http':'http://45.90.216.166:10001','https': 'https://45.90.216.166:10001'}]
 	print("\n______________________CitiLink____________________")
 	#for zalupe in range(0,6):
 	for iter in range(1, 3):   
 		print("page "+str(iter)+"-----------------------------2")
-		xax=random.uniform(1,5)
+		xax=random.uniform(1,3)
 		print(xax)
 		time.sleep(xax)
 
-		req=requests.get(f'https://www.citilink.ru/catalog/smartfony/?f=discount.any%2Crating.any%2Capple%2Chonor%2Csamsung%2Crating.any%2Capple%2Csamsung&p={iter}',headers=head[iter],proxies=prox[0])
+		req=requests.get(f'https://www.citilink.ru/catalog/smartfony/?f=discount.any%2Crating.any%2Capple%2Chonor%2Csamsung%2Crating.any%2Capple%2Csamsung&p={iter}',headers=agents[2], timeout=35)
+		#req=requests.get(f'https://2ip.ua/ru/',headers=agents[2], timeout=37)
+		costAndTags = BeautifulSoup(req.text, "html.parser").find_all(attrs={'class': 'ipblockgradient'})
 
-		print(req.status_code,'\n', '\n') #req.cookies.get_dict() )
-		
+		print(req.status_code,'\n-----------', req.request.headers, req.headers )
+		#print(costAndTags,'\n', '\n') 
+		 
+
 		b = BeautifulSoup(req.text, "html.parser")
 		costAndTags = b.find_all(attrs={'class': 'ProductCardHorizontal__price_current-price js--ProductCardHorizontal__price_current-price'})
 		print(costAndTags[0].text)
